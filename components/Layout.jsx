b827ac68491cb2bac72f71115937af28153f5ab1
Jack Jona <65679392+jackjona123@users.noreply.github.com>
2022-01-18 11:15:47 -0500

    update favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,6 +9,13 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Real Estate</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+        <meta name="msapplication-TileColor" content="#c2d5d8"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
